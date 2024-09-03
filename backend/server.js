5383aae1d1f1a82c8873ffeb0ab418ea9beb9d7a
mdsalmanfarsee <far90986@gmail.com>
2024-09-03 20:03:50 +0530

    small changes

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["http://localhost:5173"],
+        origin: [""],
         methods: ["POST", "GET"],
         credentials: true
     }
