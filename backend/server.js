009277136c1df13faab85c639b1331d09254fe19
MD SALMAN FARSEE <far90986@gmail.com>
2024-09-04 07:43:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 app.use(cors(
     {
-        origin: [""],
+        origin: ["http://localhost:5173","http://localhost:5174"],
         methods: ["POST", "GET"],
         credentials: true
     }
